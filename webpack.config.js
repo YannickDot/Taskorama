2953172f355925d1f984653ac6ee66764ee955e4
YannickDot <yannick.levif@gmail.com>
2017-05-08 22:51:41 +0200

    prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
-const {resolve, join} = require('path')
+const { resolve, join } = require('path')
 const webpack = require('webpack')
 const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = (env = {}) => {
-
   console.log('env : ', env)
   const libraryName = 'taskorama'
-  const addPlugin = (add, plugin) => add ? plugin : undefined
+  const addPlugin = (add, plugin) => (add ? plugin : undefined)
   const ifProd = plugin => addPlugin(env.prod, plugin)
   const ifDev = plugin => addPlugin(env.dev, plugin)
   const removeEmpty = array => array.filter(i => !!i)
@@ -28,22 +27,28 @@ module.exports = (env = {}) => {
     },
     module: {
       loaders: [
-        {test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/},
-        {test: /\.json$/, loaders: ['json-loader'], exclude: /node_modules/}
+        { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
+        { test: /\.json$/, loaders: ['json-loader'], exclude: /node_modules/ }
       ]
     },
     plugins: removeEmpty([
-      ifProd(new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      })),
-      ifProd(new webpack.optimize.UglifyJsPlugin({
-        mangle: true,
-        compress: {
-          screw_ie8: true,
-          warnings: false
-        }
-      })),
+      ifProd(
+        new webpack.LoaderOptionsPlugin({
+          minimize: true,
+          debug: false
+        })
+      ),
+      ifProd(
+        new webpack.optimize.UglifyJsPlugin({
+          mangle: false,
+          comments: false,
+          compress: {
+            screw_ie8: true,
+            warnings: false,
+            drop_console: true
+          }
+        })
+      ),
       new CompressionPlugin()
     ]),
     resolve: {
