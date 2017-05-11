3599f5cf2c86f280ee7783df2bdd9ec8b2022b05
YannickDot <yannick.levif@gmail.com>
2017-05-12 01:35:27 +0200

    added rollup build step

---
+++ rollup.config.browser.js
@@ -0,0 +1,17 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import butternut from 'rollup-plugin-butternut'
+
+export default {
+  entry: 'src/browser.js',
+  format: 'umd',
+  moduleName: 'taskorama',
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+    butternut()
+  ],
+  dest: 'browser.js'
+}
